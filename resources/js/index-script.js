917156d85d4fe67e926a8db71002858d3d147e41
John Wu <z328wu@uwaterloo.ca>
2021-11-01 19:26:34 -0400

    implemented create method

--- resources/js/index-script.js
+++ resources/js/index-script.js
@@ -1,12 +1,31 @@
+// HTML elements
+const create_game_button = document.getElementsByClassName("create_game_button")[0];
+
+// Wiring events
+create_game_button.addEventListener("click", e => {
+    // user wants to create new game
+    const payload = {
+        "method": "create",
+        "client_id": client_id
+    };
+
+    websocket.send(JSON.stringify(payload));
+});
+
 let client_id = null;
-let websocket = new WebSocket("ws://15.222.5.9:9090");
+let websocket = new WebSocket("ws://localhost:9090");
 websocket.onmessage = message => {
-    // message to server data
+    // received server message
     const response = JSON.parse(message.data);
     
-    // connect method
+    // receive connect response from server
     if (response.method === "connect") {
         client_id = response.client_id;
         console.log("Client id set successfully: " + client_id);
+    }
+
+    // receive create response from server
+    if (response.method === "create") {
+        console.log("Game successfully created, game id: " + response.game.id + ", balls: " + response.game.balls);
     }
 }
--- server/server.js
+++ server/server.js
@@ -6,15 +6,19 @@ const ws_server = require("websocket").server;
 
 const root_dir = path.join(__dirname, "../");
 
+// serve html index
 app = express()
 app.use(express.static(root_dir))
 app.get("/", (req,res) => res.sendFile(root_dir + "views/index.html"));
 app.listen(9091, () => console.log("Listening on port 9091..."));
 
+// create server and listen for requests
 const http_server = http.createServer();
 http_server.listen(9090, () => console.log("Listening on port 9090..."));
 
+// declare game data objects
 const clients = {};
+const games = {};
 
 const websocket_server = new ws_server({
     "httpServer": http_server
@@ -28,7 +32,24 @@ websocket_server.on("request", request => {
     connection.on("message", message => {
         // message received
         const result = JSON.parse(message.utf8Data);
-        console.log(result);
+        
+        // user wants to create new game
+        if (result.method === "create") {
+            const client_id = result.client_id;
+            const game_id = uuidv4();
+            games[game_id] = {
+                "id": game_id,
+                "balls": 20
+            };
+
+            const payload = {
+                "method": "create",
+                "game": games[game_id]
+            };
+
+            const con = clients[client_id].connection;
+            con.send(JSON.stringify(payload));
+        };
     });
 
     // generate a new client_id
